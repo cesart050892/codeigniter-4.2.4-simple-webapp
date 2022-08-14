79d8fd63b96e0ee64116d1a41c614b7b821393c3
cesart050892 <cesart050892@gmail.com>
2022-08-14 12:26:58 -0600

    part_3 >> controller new methods

--- app/Controllers/RecipesController.php
+++ app/Controllers/RecipesController.php
@@ -6,6 +6,10 @@ use App\Libraries\MyRecipes;
 
 class RecipesController extends BaseController
 {
+    /**
+     * List of recipes
+     * @return string
+     */
     public function index()
     {
         // Create an instance of our library
@@ -15,7 +19,7 @@ class RecipesController extends BaseController
         $data = [
             'page_title' => "My Recipes",
             'page_subtitle' => "I present you my favorite recipes...",
-            'recipes' => $myRecipes->getAllRecipes(),
+            'recipes' => $myRecipes->getListRecipes(),
         ];
 
         /* Each of the items in the $data array will be accessible
@@ -23,5 +27,49 @@ class RecipesController extends BaseController
          * $page_title, $page_subtitle and $recipes
          */
         return view('recipe_list', $data);
+    }
+
+    /**
+     * One recipe
+     * @param int $id
+     * @return string
+     */
+    public function recipeById(int $id)
+    {
+        // Create an instance of our library
+        $myRecipes = new MyRecipes();
+
+        $data = [];
+
+        /* Get the recipe for the id received in parameter.
+         * If the recipe does not exist, throw a page not found exception (error 404)
+         */
+        if (!$data['recipe'] = $myRecipes->getRecipeById($id)) {
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+        }
+
+        return view('recipe', $data);
+    }
+
+    /**
+     * One recipe
+     * @param string $slug
+     * @return string
+     */
+    public function recipeBySlug(string $slug)
+    {
+        // Create an instance of our library
+        $myRecipes = new MyRecipes();
+
+        $data = [];
+
+        /* Get the recipe for the slug received in parameter.
+         * If the recipe does not exist, throw a page not found exception (error 404)
+         */
+        if (!$data['recipe'] = $myRecipes->getRecipeBySlug($slug)) {
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+        }
+
+        return view('recipe', $data);
     }
 }
